5df3871d6bc176dee658605ec5f41d959bf13344
kholoodHemaid <khemaid1@smail.ucas.edu.ps>
2023-06-23 12:01:50 +0300

    hello

--- lib/Screens/Card Info__Filled.dart
+++ lib/Screens/Card Info__Filled.dart
@@ -17,18 +17,44 @@ class _CardInfo__FilledState extends State<CardInfo__Filled> {
           height: double.infinity,
       color: Colors.white,
        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         mainAxisAlignment: MainAxisAlignment.start,
          children: [
+           Padding(
+             padding: const EdgeInsets.only(top:50,left: 15),
+             child: Align(
+                 alignment: Alignment.topLeft,
+                 child: Icon(Icons.cancel_presentation_rounded)),
+           ),
+           SizedBox(height: 30,),
             Align(
               alignment: Alignment.topCenter,
               child: Container(
-               padding: EdgeInsets.only(top: 40,),
+               padding: EdgeInsets.only(top: 40,right: 30),
 
                child: ImageFiltered(
                  imageFilter: ImageFilter.blur(),
-                 child: Image.asset("images/illustration.png",width:double.infinity),
+                 child: Image.asset("images/illustration.png",width:double.infinity,),
                ),
            ),
             ),
+           RichText(
+             text: const TextSpan(
+                 text: 'New Member?\n',
+                 style: TextStyle(color: Colors.black),
+                 children: [
+                   TextSpan(
+                     text: 'Sign in',
+                     style: TextStyle(
+                       color: Colors.deepPurple,
+                       decoration: TextDecoration.underline,
+                     ),
+                   ),
+                 ]),
+           ),
+
+
+
          ],
        ),
 
